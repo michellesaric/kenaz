3f742fd74c64fc814ce5d5889d3a16ead12baf1d
„michellesaric“ <msaric03@fesb.hr>
2024-01-18 14:40:15 +0100

    Added footer newsletter

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,12 +1,13 @@
 <script setup>
 import FooterSocialMedia from "./FooterSocialMedia/FooterSocialMedia.vue";
+import FooterNewsLetter from "./FooterNewsletter/FooterNewsLetter.vue";
 </script>
 
 <template>
   <footer class="footer">
     <div class="footer__wrapper">
       <div class="footer__column"><FooterSocialMedia /></div>
-      <div class="footer__column"></div>
+      <div class="footer__column"><FooterNewsLetter /></div>
       <div class="footer__column"></div>
     </div>
   </footer>
---
+++ src/components/Footer/FooterNewsletter/FooterNewsLetter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="footer-newsletter">
+    <h2 class="footer-newsletter__title">Newsletter</h2>
+    <p class="footer-newsletter__text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus leo
+      ante...
+    </p>
+    <div class="footer-newsletter__subscribe">
+      <input
+        type="text"
+        placeholder="Your mail"
+        class="footer-newsletter__subscribe-input"
+      />
+      <button class="footer-newsletter__subscribe-button">Subscribe</button>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+@import "./_footerNewsLetter.scss"
+</style>
